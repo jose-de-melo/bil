aec023200de066032b9b66feefcbc62f16c2a517
José de Melo <josedemelo.if@gmail.com>
2019-05-27 15:00:19 -0300

    [UP] Primeiras configurações do servidor Express

--- src/config/server.js
+++ src/config/server.js
@@ -16,15 +16,38 @@ const port = 5432
  */
 const bodyParser = require('body-parser');
 const express = require('express');
-
-
-
-
-
-
-
-
+const allowCors = require('./cors');
+const queryParser = require('express-query-init');
 
 // Criando a instância inicial do servidor Express
 const serverExpress = express()
+
+
+/**
+ * Aplicando alguns middlewares ao servidor.
+ */
+
+// Trata dados de application/x-www-form-urlencoded post data (formulários)
+serverExpress.use(bodyParser.urlencoded({extended:true}));
+
+// Trata dados de application/json (JSON)
+serverExpress.use(bodyParser.json())
+
+// Faz com que todas as requisições tenham o CORS em seu cabeçalho
+serverExpress.use(allowCors)
+
+// Converte valores passados como string que seriam números para o seu tipo corretamente.
+serverExpress.use(queryParser())
+
+
+// Rodando o servidor
+serverExpress.listen(port, function(){
+    console.log("Servidor rodando na porta ${port}")
+})
+
+// Após isso, o servidor estará todando em http://localhost:5432/, porém, sem rotas.
+
+
+// Exporta o servidor para que ele possa ser usado em outros lugares da aplicação. 
+module.exports = serverExpress
 
