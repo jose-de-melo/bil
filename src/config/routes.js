f3629f8fa7cc8931cb55457ae12a518412370d79
José de Melo <josedemelo.if@gmail.com>
2019-08-01 14:06:47 -0300

    [UP] Finalizando alteracao da rota /docs

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,35 +1,13 @@
 /**
- * Importando o JSON com as informações sobre cada documento.
- */
-const docs = require('../api/docs/docsList.json')
-
-
-/**
  * Biblioteca para criar os serviços de uma API
  */
 const express = require('express')
-
-// Biblioteca usada para listar o conteúdo de diretórios
-const fs = require('fs')
-
-
 
 /**
  * Importando as bibliotecas chatService e chatBot
  */
 const chatService = require('../api/chat/chatService')
 const chatBot = require('../api/chatBot/chatBot')
-
-let typesDocs = ['checklist', 'manual', 'open-process', 'other']
-
-
-/**
- * Definindo o caminho do diretório onde estão os arquivos
- * 
- * __dirname é uma string que armazena o diretório onde está o arquivo
- */
-const path = __dirname.slice(0, __dirname.indexOf('/src/')) + process.env.DOCS_PATH
-
 
 /**
  * Módulo para criar as rotas para o servidor recebido por parâmetro
@@ -74,33 +52,23 @@ module.exports = function (server) {
      * Rota para fornecer arquivos públicos.
      */
     api.get('/docs', (req, res) =>{
-        const resp = {} 
-        fs.readdir(path, (err, paths) => {
-            if(err){
-                console.log(err)
-                res.status(500).json({'message': 'Erro! Não foi possível listar os arquivos disponíveis.', 'erros': err})
-            }
-            else{
-                resp['files'] = []
-                if(paths.length > 0){
-                    let file
+        try {
+            /**
+            * Importando o JSON com as informações sobre cada documento.
+            */
+            const documents = require('../api/docs/docsList.json')
 
-                    paths.forEach(element => {
-                        file = {
-                            'name': element.slice(element.indexOf('- ')+2),
-                            'url': process.env.SERVICE_URL + '/documents/' + element,
-                            'type_doc': typesDocs[parseInt(element.slice(0, element.indexOf(' -'))) - 1],
-                            'extension': element.slice(element.lastIndexOf('.') + 1, element.length),
-                            'description': 'Se tratando de clubes, o Galo é o maior de todos e isso não há o que discutir.'
-                        }
+            documents.list.forEach(doc => {
+                doc['url'] = process.env.SERVICE_URL + '/documents/' + doc.name + doc.extension
+            })
 
-                        resp['files'].push(file)
-                    });
+            res.status(200).json(documents)
 
-                }
-                res.status(200).json(resp)
-            }
-        })
+
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({'message': 'Erro! Não foi possível listar os arquivos disponíveis.'})
+        }
     })
 
 
