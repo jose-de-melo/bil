4c9186ca8e6ad07287609d4a64207bd8056ff138
José de Melo <josedemelo.if@gmail.com>
2019-08-02 17:22:33 -0300

    [UP] Settings deploy

--- src/config/database.js
+++ src/config/database.js
@@ -7,6 +7,11 @@ const mongoose = require('mongoose')
 
 
 /**
+ * Obtém as variáveis de ambiente do arquivo '.env'
+ */
+require('dotenv').config()
+
+/**
  * Definindo usePushEach (inserir múltiplos valores em um array) como padrão para todos os esquemas.
  */
 mongoose.plugin(schema => { schema.options.usePushEach = true });
@@ -15,7 +20,9 @@ mongoose.plugin(schema => { schema.options.usePushEach = true });
 // Tornando a conexão global para que a mesma seja usada em outras partes da aplicação.
 mongoose.Promise = global.Promise
 
+const uri = process.env.MONGODB_URI || 'mongodb://localhost/db-bil'
+
 
 // Estabelecendo a conexão e exportando 
-module.exports = mongoose.connect('mongodb://localhost/db-bil', {useMongoClient: true})
+module.exports = mongoose.connect(uri, {useMongoClient: true})
 
